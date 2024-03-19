047e1c0c1d9d16d61888f2f227d068c5efd79c22
Parwinder Singh <parwindersingh@techmarcos.com>
2024-03-19 17:42:40 +0530

    Plugin updated

--- crud.php
+++ crud.php
@@ -14,46 +14,50 @@ if (!defined('ABSPATH')) {
 
 
 // Plugin Activation Hook
-register_activation_hook( __FILE__, 'create_table' );
+register_activation_hook(__FILE__, 'create_table');
 
-function create_table() {
-    global $wpdb;    
+function create_table()
+{
+    global $wpdb;
     // Define table name
-    $table_name = $wpdb->prefix . 'student';    
+    $table_name = $wpdb->prefix . 'student';
     // SQL to create table
     $sql = "CREATE TABLE IF NOT EXISTS $table_name (
         id MEDIUMINT(9) NOT NULL AUTO_INCREMENT,
         student_name VARCHAR(50) NOT NULL,
         student_email VARCHAR(50) NOT NULL,
         class VARCHAR(50) NOT NULL,
+        image VARCHAR(100) NOT NULL,
         roll_no INT NOT NULL, 
         PRIMARY KEY (id)
     )";
-    
+
     // Include upgrade.php for dbDelta()
-    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );    
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
     // Create table
-    dbDelta( $sql );
+    dbDelta($sql);
 }
 // Plugin Deactivation Hook
-register_deactivation_hook( __FILE__, 'delete_table' );
+register_deactivation_hook(__FILE__, 'delete_table');
 
-function delete_table() {
+function delete_table()
+{
     global $wpdb;
-    
+
     // Define table name
     $table_name = $wpdb->prefix . 'student';
-    
+
     // SQL to drop table
     $sql = "DROP TABLE IF EXISTS $table_name";
-    
+
     // Execute SQL
-    $wpdb->query( $sql );
+    $wpdb->query($sql);
 }
 
 
 // Step 1: Add an Admin Menu Page
-function student_form_menu() {
+function student_form_menu()
+{
     add_menu_page(
         'Student Form',
         'Student Form',
@@ -64,7 +68,8 @@ function student_form_menu() {
 }
 add_action('admin_menu', 'student_form_menu');
 
-function custom_admin_form_enqueue_scripts($hook) {
+function custom_admin_form_enqueue_scripts($hook)
+{
     if ($hook != 'toplevel_page_custom-admin-form') {
         return;
     }
@@ -74,56 +79,55 @@ function custom_admin_form_enqueue_scripts($hook) {
 }
 add_action('admin_enqueue_scripts', 'custom_admin_form_enqueue_scripts');
 
-require_once plugin_dir_path( __FILE__ ) . 'my_database_handler.php';
+require_once plugin_dir_path(__FILE__) . 'my_database_handler.php';
 
 
 
-function student_form_page() {  
+function student_form_page()
+{
     $database_handler = new MyDatabaseHandler('student');
-    $students = $database_handler->fetch_data(); 
-  require_once plugin_dir_path( __FILE__ ) . 'student-listing.php';    
- //  require_once plugin_dir_path( __FILE__ ) . 'student_form.html';  
+    $students = $database_handler->fetch_data();
+    require_once plugin_dir_path(__FILE__) . 'student-listing.php';
+    //  require_once plugin_dir_path( __FILE__ ) . 'student_form.html';  
 }
 
 
-function handle_custom_admin_form_submission() {
-    require_once plugin_dir_path( __FILE__ ) . 'my_database_handler.php';
+function handle_custom_admin_form_submission()
+{
+    require_once plugin_dir_path(__FILE__) . 'my_database_handler.php';
     $database_handler = new MyDatabaseHandler('student');
     if (isset($_POST['data'])) {
-            if($_POST['data']['id']){
-                    $where = array( 'id' => $_POST['data']['id'] );
-                    $database_handler->delete_data($where);
+        if ($_POST['data']['id']) {
+            $where = array('id' => $_POST['data']['id']);
+            $database_handler->delete_data($where);
             $response = array(
-                'success' => true, 
+                'success' => true,
                 'message' => 'Student data deleted successfully' // Optional message
             );
-            }   
-         parse_str($_POST['data'], $formData);
-        if($formData){
-            if(!empty($formData['id'])){
+        }
+        parse_str($_POST['data'], $formData);
+        if ($formData) {
+            if (!empty($formData['id'])) {
 
-                        $where = array(
-                            'id' => $formData['id'] 
-                        );
-                        $database_handler->update_data($formData,$where);                 
-                        $response = array(
-                            'success' => true, 
-                            'message' => 'Student data Updated successfully' // Optional message
-                        );
-                    }else{  
-                        
-                        $database_handler->insert_data($formData);
-                        $response = array(
-                            'success' => true, 
-                            'message' => $formData // Optional message
-                        );
-                    }
+                $where = array(
+                    'id' => $formData['id']
+                );
+                $database_handler->update_data($formData, $where);
+                $response = array(
+                    'success' => true,
+                    'message' => 'Student data Updated successfully' // Optional message
+                );
+            } else {
 
+                $database_handler->insert_data($formData);
+                $response = array(
+                    'success' => true,
+                    'message' => $formData // Optional message
+                );
             }
-        
-    wp_send_json($response);
+        }
+
+        wp_send_json($response);
     }
 }
 add_action('wp_ajax_custom_admin_form_submit', 'handle_custom_admin_form_submission');
-
-
--- js/custom-admin-ajax-script.js
+++ js/custom-admin-ajax-script.js
@@ -1,81 +1,76 @@
-var student="";
-jQuery(document).ready(function($) {
-    var student="";
-    jQuery(document).ready(function($){
-        $("#student_form").hide();
-        $("#form").click(function(){
-          $("#listing").hide();
-          $("#student_form").show();
-        });
-        $(".delete-student").click(function(){
-            
-            if(confirm("Are you sure you want to delete this?")){
-            var student=data[$(this).attr("data-id")];
-            const form_data ={ id: student['id'] };
-                
+var student = "";
+jQuery(document).ready(function ($) {
+  var student = "";
 
-       console.log(form_data);
-        $.ajax({
-            type: 'POST',
-            url: ajax_object.ajax_url,
-            data: {
-                action: 'custom_admin_form_submit',
-                data:form_data // Get input data value
-            },
-            success: function(response) {
-                if (response.success) {
-                    location.reload();
-                } 
-            }
-        });
-            }
-            else{
-                return false;
-            }
+  $("#student_form").hide();
+  $("#form").click(function () {
+    $("#listing").hide();
+    $("#student_form").show();
+  });
+  $(".delete-student").click(function () {
+    if (confirm("Are you sure you want to delete this?")) {
+      var student = data[$(this).attr("data-id")];
+      const form_data = { id: student["id"] };
 
-        });
-         $("#cancel").click(function(){
-
-          $("#listing").show();
-          $("#student_form").hide();
-        });
-        $(".edit-form").click(function(){
-            $("#listing").hide();
-            $("#student_form").show();           
-            var student=data[$(this).attr("data-id")];
-            $('#name').val(student['student_name']); 
-            $('#email').val(student['student_email']); 
-            $('#class').val(student['class']); 
-            $('#roll_no').val(student['roll_no']); 
-            $('#id').val(student['id']); 
-            // console.log( student);
-          });
+      console.log(form_data);
+      $.ajax({
+        type: "POST",
+        url: ajax_object.ajax_url,
+        data: {
+          action: "custom_admin_form_submit",
+          data: form_data, // Get input data value
+        },
+        success: function (response) {
+          if (response.success) {
+            location.reload();
+          }
+        },
       });
-    $('#custom-admin-form').submit(function(e) { 
-        e.preventDefault();
-        var name=$('#name').val();
-        var email=$('#email').val();
-        var roll_no=$('#roll_no').val();
-        var s_class =$('#class').val();
-        if(name && email && roll_no && s_class){
-            var form_data = $(this).serialize();
-                    console.log(name);
-                    
-                    $.ajax({
-                        type: 'POST',
-                        url: ajax_object.ajax_url,
-                        data: {
-                            action: 'custom_admin_form_submit',
-                            data:form_data // Get input data value
-                        },
-                        success: function(response) {
-                            if (response.success) {
-                                location.reload();
-                            } 
-                        }
-                    });
-        }else{
-        alert('Please fill all fields');
-        }       
-    });
+    } else {
+      return false;
+    }
+  });
+  $("#cancel").click(function () {
+    $("#listing").show();
+    $("#student_form").hide();
+  });
+  $(".edit-form").click(function () {
+    $("#listing").hide();
+    $("#student_form").show();
+    var student = data[$(this).attr("data-id")];
+    $("#name").val(student["student_name"]);
+    $("#email").val(student["student_email"]);
+    $("#class").val(student["class"]);
+    $("#roll_no").val(student["roll_no"]);
+    $("#id").val(student["id"]);
+    // console.log( student);
+  });
+
+  $("#custom-admin-form").submit(function (e) {
+    e.preventDefault();
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var roll_no = $("#roll_no").val();
+    var s_class = $("#class").val();
+    if (name && email && roll_no && s_class) {
+      var form_data = $(this).serialize();
+      console.log(name);
+
+      $.ajax({
+        type: "POST",
+        url: ajax_object.ajax_url,
+        data: {
+          action: "custom_admin_form_submit",
+          data: form_data, // Get input data value
+        },
+        success: function (response) {
+          if (response.success) {
+            location.reload();
+          }
+        },
+      });
+    } else {
+      alert("Please fill all fields");
+    }
+  });
 });
